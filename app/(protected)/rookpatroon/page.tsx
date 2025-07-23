a10f16e1f4fed384581ac567f496e9ffe76c126a
Alex Borgman <info@webdesignborgman.nl>
2025-07-23 21:06:19 +0200

    feat: Add RookpatroonPage component with initial layout

---
+++ app/(protected)/rookpatroon/page.tsx
@@ -0,0 +1,8 @@
+export default function RookpatroonPage() {
+  return (
+    <main className="p-4">
+      <h1 className="text-2xl font-bold mb-4">Rookpatroon</h1>
+      {/* Voeg hier je rookpatroon content toe */}
+    </main>
+  );
+}
